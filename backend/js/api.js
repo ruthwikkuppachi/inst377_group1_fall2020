003cdf437f064449482a388e3598821e9971a32c
Maya Brenner-Letich <mayabren@terpmail.umd.edu>
2020-12-05 08:17:08 -0500

    Update api.js

--- backend/js/api.js
+++ backend/js/api.js
@@ -13,5 +13,15 @@ async function getData() {
             let zip = json.zip_code;
             let name = json.payee_name;
             let amount = json.amount;
+
+            //turn json into sql with columns zip, name, amount, agency - Creates table with all 4 columns that can be queried
+
+            //query for every row that zip/name/agency (whatever was inputed into the search bar)
+
+            //sum up the amounts section and add to console
+
+
+            //*ALLOWS USER TO SEARCH FOR TOTAL COSTS BY ZIP, NAME, AND AGENCY
+            //TO GROUP: we need either three search bars, one zip, name, and agency
         })
 }
