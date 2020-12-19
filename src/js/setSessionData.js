8a6ee3d207ac22d33888464bcf5d201ef8942e6e
ruthwik11 <51654907+ruthwik11@users.noreply.github.com>
2020-12-18 23:46:58 -0500

    Update

---
+++ src/js/setSessionData.js
@@ -0,0 +1,54 @@
+
+const main = document.getElementById("main");
+function getResources() {
+    let resource = document.getElementById("agency").value;
+
+    if ((typeof resource !== "undefined") & (resource !=="")) {
+        let resourceURL = "https://data.princegeorgescountymd.gov/resource/uh6s-izyj.json?agency=" + resource;
+
+        fetch(resourceURL)
+        .then((results) => {return results.json();})
+  
+        .then((route) => {
+            let payee_name = route.payee_name;
+            let agency = route.agency;
+            let zip_code = route.zip_code;
+            let amount = route.amount
+            let payment_description = route.payment_description;
+            let nothing = "Nothing Set!";
+  
+            sessionStorage.setItem("title", payee_name);
+            sessionStorage.setItem("lat_max", agency);
+            sessionStorage.setItem("lat_min", zip_code);
+            sessionStorage.setItem("lon_max", amount);
+            sessionStorage.setItem("lon_min", payment_description);
+
+            string = JSON.stringify(route)
+            document.getElementById("main").innerHTML = string;
+            
+          }
+        )
+        .catch((err) => {
+          console.log(err);
+          main.innerHTML = "Invalid bus route";
+          sessionStorage.setItem("title", nothing);
+          sessionStorage.setItem("lat_max", nothing);
+          sessionStorage.setItem("lat_min", nothing);
+          sessionStorage.setItem("lon_max", nothing);
+          sessionStorage.setItem("lon_min", nothing);
+        
+
+        }
+        );
+    } else {
+      main.innerHTML = "No value provided";
+      sessionStorage.setItem("title", nothing);
+      sessionStorage.setItem("lat_max", nothing);
+      sessionStorage.setItem("lat_min", nothing);
+      sessionStorage.setItem("lon_max", nothing);
+      sessionStorage.setItem("lon_min", nothing);
+    }
+    
+  }
+
+  
